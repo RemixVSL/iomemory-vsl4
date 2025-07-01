ded272c4b675de36e56b0854e6c7fe299bfb346b
Funs <fozzielumpkins@gmail.com>
2025-07-01 08:32:15 -0500

    set correct desc

--- root/usr/src/iomemory-vsl4-4.3.7/mod.c
+++ root/usr/src/iomemory-vsl4-4.3.7/mod.c
@@ -1,5 +1,5 @@
 #include <linux/module.h>
 
 MODULE_AUTHOR("Funs Kessen <funs@barred.org>");
-MODULE_DESCRIPTION("vsl3 driver, you know, for vsl3 devices");
+MODULE_DESCRIPTION("vsl4 driver, you know, for vsl4 devices");
 MODULE_INFO(supported, "external");
